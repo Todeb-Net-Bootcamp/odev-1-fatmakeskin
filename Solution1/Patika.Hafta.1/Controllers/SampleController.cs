d520c0c9742b7e8d99362d48165ceb1baa815951
fatmakeskin <72805408+fatmakeskin@users.noreply.github.com>
2022-06-24 21:41:58 +0300

    first commit

---
+++ Solution1/Patika.Hafta.1/Controllers/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patika.Hafta._1.Controllers
+{
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+
+    }
+}
